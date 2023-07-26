ea96ba48f2aec57c6609c36b1a0e636fb28f0843
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-07-26 14:31:12 -0700

    Add d10()

---
+++ rolls/__init__.py
@@ -0,0 +1,12 @@
+"""Handle rolls for both game lines."""
+
+from numpy.random import default_rng
+
+_rng = default_rng()  # numpy's default RNG is PCG64 (superior to builtin)
+
+
+def d10(count: int | None = None) -> int | list[int]:
+    """Roll one or many d10s."""
+    if count is None:
+        return int(_rng.integers(1, 11))
+    return list(map(int, _rng.integers(1, 11, count)))
---
+++ tests/rolls/test_rng.py
@@ -0,0 +1,19 @@
+"""Miscellaneous roll tests."""
+
+import pytest
+
+from rolls import d10
+
+
+def test_single_d10():
+    for _ in range(10000):
+        num = d10()
+        assert isinstance(num, int)
+        assert 1 <= num <= 10
+
+
+@pytest.mark.parametrize("count", [n for n in range(1, 101)])
+def test_multiple_d10s(count):
+    dice = d10(count)
+    assert len(dice) == count
+    assert all(1 <= d <= 10 for d in dice)
