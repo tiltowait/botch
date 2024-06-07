95eec0e5e66360c0db477831f850c1fd19e87316
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-06-06 22:20:54 -0700

    Export Experience

--- botch/characters/__init__.py
+++ botch/characters/__init__.py
@@ -1,4 +1,12 @@
 """The characters package defines various PC/NPC objects and functions."""
 
 from botch.characters import wod
-from botch.characters.base import Character, Damage, GameLine, Grounding, Splat, Trait
+from botch.characters.base import (
+    Character,
+    Damage,
+    Experience,
+    GameLine,
+    Grounding,
+    Splat,
+    Trait,
+)
