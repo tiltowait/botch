ceb09588b8e0c6855cb2966b9a4245b7c17be52b
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-06-07 21:30:26 -0700

    Add character option

--- botchcord/options.py
+++ botchcord/options.py
@@ -1,6 +1,11 @@
 """Commonly used slash command options and helpers."""
 
+import logging
+
+import discord
 from discord.commands import Option, OptionChoice
+
+import core
 
 
 def promoted_choice(
@@ -31,3 +36,59 @@ def promoted_choice(
         return func
 
     return decorator
+
+
+def character(description="The character to use", required=False, param="character"):
+    """A command decorator letting users choose a character."""
+
+    def decorator(func):
+        func.__annotations__[param] = Option(
+            str,
+            description,
+            name=param,
+            autocomplete=_available_characters,
+            required=required,
+        )
+        return func
+
+    return decorator
+
+
+async def _available_characters(ctx: discord.AutocompleteContext):
+    """Generate a list of the user's available characters."""
+    logger = logging.getLogger("CHAR_OPTION")
+
+    if (guild := ctx.interaction.guild) is None:
+        return []
+
+    # Check if they're looking up a player and have lookup permissions
+    user = ctx.interaction.user
+    spcs = []
+
+    if (owner := (ctx.options.get("owner") or ctx.options.get("current_owner"))) is not None:
+        if owner != user.id and not user.guild_permissions.administrator:
+            return [OptionChoice("You do not have admin permissions", "")]
+    else:
+        owner = user.id
+
+        if user.guild_permissions.administrator:
+            # Add SPCs
+            spcs = await core.cache.fetchnames(guild.id, ctx.bot.user.id)
+            logger.info(
+                "%s: admin %s fetched %s SPCs",
+                ctx.interaction.guild.name,
+                ctx.interaction.user.name,
+                len(spcs),
+            )
+
+    chars = await core.cache.fetchnames(guild.id, int(owner))
+    chars.extend(spcs)
+
+    name_search = ctx.value.casefold()
+
+    found_chars = [name for name in chars if name.casefold().startswith(name_search or "")]
+
+    if len(found_chars) > 25:
+        instructions = "Keep typing ..." if ctx.value else "Start typing a name."
+        return [OptionChoice(f"Too many characters to display. {instructions}", "")]
+    return found_chars
