159d154f476a8651c28a1f8cfd2b0add66143fb6
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-06-08 19:35:28 -0700

    Remove client and db globals
    We shouldn’t be directly manipulating the database outside of beanie documents, so we don’t need the globals.
    
    Removing the globals also ensures unit tests don’t need the key, an issue which was causing CircleCI builds to fail.

--- db.py
+++ db.py
@@ -15,11 +15,11 @@ load_dotenv()
 logger = logging.getLogger("db")
 logger.info("Initializing database connection")
 
-client = AsyncIOMotorClient(os.environ["MONGO_URL"])
-db = client[os.environ["MONGO_DB"]]
-
 
 async def init():
     """Initialize the database."""
     logger.info("Initializing beanie")
+
+    client = AsyncIOMotorClient(os.environ["MONGO_URL"])
+    db = client[os.environ["MONGO_DB"]]
     await init_beanie(database=db, document_models=[Character, wod.Vampire, wod.Mortal, Roll])
