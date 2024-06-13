b3b4f4bf319eb579c5801bbcd79de7dbb7e14b28
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-06-12 18:47:03 -0700

    Fix argument types

--- botchcord/utils/text.py
+++ botchcord/utils/text.py
@@ -1,31 +1,31 @@
 """Discord text utilities."""
 
 
-def b(text: str) -> str:
+def b(text: object) -> str:
     """Bold text."""
     return f"**{text}**"
 
 
-def i(text: str) -> str:
+def i(text: object) -> str:
     """Italic text."""
     return f"*{text}*"
 
 
-def bi(text: str) -> str:
+def bi(text: object) -> str:
     """Bolditalic text."""
     return f"***{text}***"
 
 
-def u(text: str) -> str:
+def u(text: object) -> str:
     """Underlined text."""
     return f"__{text}__"
 
 
-def m(text: str) -> str:
+def m(text: object) -> str:
     """Monospaced text."""
     return f"`{text}`"
 
 
-def c(text: str) -> str:
+def c(text: object) -> str:
     """Code-fenced text."""
     return f"```{text}```"
