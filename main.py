57413ca98795416936524e14ae7c89a1fa29f60e
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-10-11 23:27:10 -0700

    asyncio is unnecessary

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 """Botch: A Discord dice bot and character manager for WoD and CofD."""
 
-import asyncio
-
 from bot import BotchBot
 from config import BOTCH_TOKEN
 
-bot = BotchBot()
-bot.load_cogs()
+
+def main():
+    assert BOTCH_TOKEN is not None
+    bot = BotchBot()
+    bot.load_cogs()
+    bot.run(BOTCH_TOKEN)
+
 
 if __name__ == "__main__":
-    asyncio.run(bot.run(BOTCH_TOKEN))
+    main()
