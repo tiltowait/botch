b6415f791409ce18f00e6c21ace4ab73108dc4bb
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-10-13 10:46:33 -0700

    Add tracker increment and decrement

--- botchcord/character/__init__.py
+++ botchcord/character/__init__.py
@@ -1,7 +1,7 @@
 """Character commands interface."""
 
-from botchcord.character import traits
+from botchcord.character import adjust, traits
 from botchcord.character.delete import delete
 from botchcord.character.display import display
 
-__all__ = ("delete", "display", "traits")
+__all__ = ("adjust", "delete", "display", "traits")
