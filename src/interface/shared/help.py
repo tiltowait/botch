dc5002a740475046587928cacbac70bd04040649
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-10-26 10:13:06 -0700

    Show cog entries in paginator

--- src/interface/shared/help.py
+++ src/interface/shared/help.py
@@ -2,8 +2,9 @@
 
 import discord
 from discord import slash_command
+from discord.cog import Cog
 from discord.commands.core import SlashCommand
-from discord.ext.commands import Cog
+from discord.ext.pages import Page, PageGroup, Paginator
 
 from bot import AppCtx, BotchBot
 
@@ -19,16 +20,36 @@ class HelpCog(Cog, name="Help"):
     @slash_command(name="help")
     async def help_command(self, ctx: AppCtx):
         """View command help. It's ... what you're looking at right now."""
-        cog = self.bot.get_cog("Macros")
-        assert cog is not None
+        page_groups = []
+        for cog_name, cog in sorted(self.bot.cogs.items()):
+            embeds = self._generate_cog_embeds(cog)
+            if embeds is None:
+                continue
+            page = Page(embeds=embeds)  # type: ignore
+            page_groups.append(PageGroup(pages=[page], label=cog_name))
+
+        paginator = Paginator(
+            pages=page_groups,
+            disable_on_timeout=True,
+            show_menu=True,
+            use_default_buttons=False,
+        )
+        await paginator.respond(ctx.interaction, ephemeral=True)
+
+    def _generate_cog_embeds(self, cog: Cog) -> list[discord.Embed] | None:
+        """Generate the embed for a cog's PageGroup. It's returned as a list of
+        embeds because Page expects a list."""
         commands = [cmd for cmd in cog.walk_commands() if isinstance(cmd, SlashCommand)]
+        if not commands:
+            return None
+
         commands.sort(key=lambda c: c.qualified_name)
 
         entries = map(self._generate_command_entry, commands)
         embed = discord.Embed(title=cog.qualified_name, description=cog.description)
         embed.add_field(name="Commands", value="\n".join(entries))
 
-        await ctx.respond(embed=embed, ephemeral=True)
+        return [embed]
 
     def _generate_command_entry(self, command: SlashCommand) -> str:
         """Generates a help entry for a command."""
