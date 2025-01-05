7de1403e3aa9307e9f4abf31bfb61760cf844d58
tiltowait <208040+tiltowait@users.noreply.github.com>
2025-01-04 19:42:10 -0800

    Respect guild accessibility setting

--- src/botchcord/settings.py
+++ src/botchcord/settings.py
@@ -1,13 +1,16 @@
 """Various settings handlers."""
 
-import discord
+from bot import AppCtx
 
 
-async def accessibility(ctx: discord.ApplicationContext) -> bool:
+async def accessibility(ctx: AppCtx) -> bool:
     """Whether to use accessibility mode for the current operation."""
-    return False  # TODO: Implement!
+    guild = await ctx.bot.guild_cache.fetch(ctx.guild, create=True)
+
+    # TODO: Implement user settings if guild accessibility is false
+    return guild.settings.accessibility
 
 
-async def use_emojis(ctx: discord.ApplicationContext) -> bool:
+async def use_emojis(ctx: AppCtx) -> bool:
     """Whether to use emojis."""
     return not await accessibility(ctx)
