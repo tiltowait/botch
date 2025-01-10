bec8ab5289491bc9c3bba206ea39a68834e1a0b1
tiltowait <208040+tiltowait@users.noreply.github.com>
2025-01-09 17:46:02 -0800

    Update settings logic

--- src/botchcord/settings.py
+++ src/botchcord/settings.py
@@ -6,9 +6,11 @@ from bot import AppCtx
 async def accessibility(ctx: AppCtx) -> bool:
     """Whether to use accessibility mode for the current operation."""
     guild = await ctx.bot.guild_cache.fetch(ctx.guild, create=True)
+    if guild.settings.accessibility:
+        return True
 
-    # TODO: Implement user settings if guild accessibility is false
-    return guild.settings.accessibility
+    user = await ctx.bot.user_store.fetch(ctx.author.id)
+    return user.settings.accessibility
 
 
 async def use_emojis(ctx: AppCtx) -> bool:
--- tests/botchcord/test_settings.py
+++ tests/botchcord/test_settings.py
@@ -2,12 +2,44 @@
 reminder to write thorough tests once the settings system is written, since
 they will fail once that work is done."""
 
+from unittest.mock import AsyncMock, Mock, patch
+
+import pytest
+
+from bot import AppCtx
 from botchcord import settings
 
 
-async def test_accessibility(ctx):
+async def test_accessibility(ctx: AppCtx):
     assert await settings.accessibility(ctx) is False
 
 
-async def test_use_emojis(ctx):
+async def test_use_emojis(ctx: AppCtx):
     assert await settings.use_emojis(ctx) != await settings.accessibility(ctx)
+
+
+@pytest.mark.parametrize(
+    "guild_a11y,user_a11y,a11y",
+    [
+        (False, False, False),
+        (True, False, True),
+        (False, True, True),
+        (True, True, True),
+    ],
+)
+async def test_settings_logic(ctx: AppCtx, guild_a11y: bool, user_a11y: bool, a11y: bool):
+    guild = Mock()
+    guild.settings.accessibility = guild_a11y
+
+    user = Mock()
+    user.settings.accessibility = user_a11y
+
+    with (
+        patch.object(ctx.bot.guild_cache, "fetch", new_callable=AsyncMock) as guild_fetch,
+        patch.object(ctx.bot.user_store, "fetch", new_callable=AsyncMock) as user_mock,
+    ):
+        guild_fetch.return_value = guild
+        user_mock.return_value = user
+
+        assert await settings.accessibility(ctx) == a11y
+        assert await settings.use_emojis(ctx) != a11y
